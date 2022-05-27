f6cc4d7efa80d9af1bafa33660fe7e528ce7c624
lmiskelis <lukas.miskelis@gmail.com>
2022-05-27 13:43:20 +0300

    init

---
+++ script.js
@@ -0,0 +1,46 @@
+/* ------------------------------ TASK 6 -----------------------------------
+Turimas "users" masyvas. 
+
+Parašykite funckijas, kurios atlikas nurodytas užduotis
+1. funkcija "getUsersNames" -  kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann Smith'..]
+2. funkcija "getUserAverageAge" - kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" amžiaus visurkį kaip skaičių.
+-------------------------------------------------------------------------- */
+
+const users = [
+  { id: '1', name: 'John Smith', age: 20 },
+  { id: '2', name: 'Ann Smith', age: 24 },
+  { id: '3', name: 'Tom Jones', age: 31 },
+  { id: '4', name: 'Rose Peterson', age: 17 },
+  { id: '5', name: 'Alex John', age: 25 },
+  { id: '6', name: 'Ronald Jones', age: 63 },
+  { id: '7', name: 'Elton Smith', age: 16 },
+  { id: '8', name: 'Simon Peterson', age: 30 },
+  { id: '9', name: 'Daniel Cane', age: 51 },
+];
+
+function getUsersNames(arr){
+  var vardai= arr.map(x=>x.name)
+  
+  return vardai
+}
+
+
+
+console.log(getUsersNames(users))
+
+
+
+function getUserAverageAge(arr){
+  var suma=0
+  for(var x of arr){
+    
+    suma +=  x.age 
+    
+  }
+ 
+  var vidurkis=suma/ arr.length
+   return vidurkis
+}
+console.log(` vidurkis yra ${getUserAverageAge(users)} metu`)
